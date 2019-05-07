9e514c5e9bf8f77fbef4a781ff021a3b65c9c750
Matt <mattmartell07@gmail.com>
2019-05-07 18:05:34 -0500

    initial commit

---
+++ index.ts
@@ -0,0 +1,42 @@
+import domains from './domains';
+var http = require('https');
+
+const apiKey = '';
+const fs = require('fs');
+var writer = fs.createWriteStream('./output/results.txt', {flags: 'a'});
+
+
+function checkAllDomains() {
+    domains.forEach(d => {
+        logDomainAvailability(d);
+        
+    });
+}
+
+function logDomainAvailability(name: string) {
+    http.get(`https://domain-availability-api.whoisxmlapi.com/api/v1?apiKey=${apiKey}&domainName=${name}`, (resp: any) => {
+        let data = '';
+
+        // A chunk of data has been recieved.
+        resp.on('data', (chunk: any) => {
+          data += chunk;
+        });
+      
+        // The whole response has been received. Log the result.
+        resp.on('end', () => {
+            const response = JSON.parse(data);
+            if (response.DomainInfo) {
+                const availability = `domain: ${response.DomainInfo.domainName}, availability: ${response.DomainInfo.domainAvailability} \r\n`;
+                console.log(availability);
+                writer.write(availability);
+            } else {
+                console.log(response);
+            }
+        });
+      
+    }).on("error", (err: any) => {
+        console.log("Error: " + err.message);
+    });
+}
+
+checkAllDomains();
